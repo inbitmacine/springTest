e7d51e80118df357651d10e1507a8edfdd137a58
user <user@DESKTOP-2BFML6M>
2020-11-26 16:49:03 +0900

    微改修

--- FirstSp/src/main/java/com/example/domain/User.java
+++ FirstSp/src/main/java/com/example/domain/User.java
@@ -10,7 +10,7 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Entity
-@Table(name = "customer")
+@Table(name = "test")
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
--- FirstSp/src/main/java/com/example/first/FirstControler.java
+++ FirstSp/src/main/java/com/example/first/FirstControler.java
@@ -1,8 +1,7 @@
 package com.example.first;
+
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.io.ResourceLoader;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -10,8 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class FirstControler {
-	@Autowired
-	protected ResourceLoader resourceLoader;
+	
 	@RequestMapping("/index")
 	public String index(Model model) {
 		
